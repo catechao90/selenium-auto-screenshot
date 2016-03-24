2e40908586de086cdcdf74f9b5fc9f4c63e30b70
cate.zhao <cate.zhao@tpooo.com>
2016-03-24 11:36:09 +0800

    removed js function

--- selenium-ie/ie.py
+++ selenium-ie/ie.py
@@ -14,28 +14,8 @@ def capture(url, save_fn="./public/IE8.png"):
     browser = webdriver.Ie() 
     browser.set_window_size(1920, 900)
     browser.get(url)
-    browser.execute_script("""
-        (function () {
-            var y = 0;
-            var step = 100;
-            window.scroll(0, 0);
  
-            function f() {
-                if (y < document.body.scrollHeight) {
-                    y += step;
-                    window.scroll(0, y);
-                    setTimeout(f, 50);
-                } else {
-                    window.scroll(0, 0);
-                    document.title += "scroll-done";
-                }
-            }
- 
-            setTimeout(f, 1000);
-        })();
-    """)
- 
-    for i in xrange(30):
+    for i in xrange(10):
         if "scroll-done" in browser.title:
             break
         time.sleep(1)

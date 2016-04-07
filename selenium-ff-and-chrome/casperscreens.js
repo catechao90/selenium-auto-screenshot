90251c5c8dbceca7165bb5ac3a761155c03ae4df
cate.zhao <cate.zhao@tpooo.com>
2016-04-07 17:32:17 +0800

    updated logic and layout

--- selenium-ff-and-chrome/casperscreens.js
+++ selenium-ff-and-chrome/casperscreens.js
@@ -2,18 +2,12 @@ var casper = require('casper').create({
   verbose: false,
   logLevel: "debug"
 });
-var screenshotDateTime = Date.parse(new Date());
+
 var viewports = require('./const').viewports;
 
 var imgList = [];
-if (casper.cli.args.length < 1) {
-  casper
-    .echo("Usage: $ casperjs casperscreens.js http://example.com")
-    .exit(1)
-  ;
-} else {
-  screenshotUrl = casper.cli.args[0];
-}
+screenshotUrl = casper.cli.args[0];
+screenshotDateTime = casper.cli.args[1];
 
 casper.start(screenshotUrl, function() {
   this.echo('Current location is ' + this.getCurrentUrl(), 'info');
@@ -24,20 +18,20 @@ casper.each(viewports, function(casper, viewport) {
     	this.viewport(viewport.viewport.width, viewport.viewport.height);
   	});
   	this.thenOpen(screenshotUrl, function() {
-    	this.wait(5000);
+    	this.wait(4000);
   	});
   	this.then(function(){
 	    // this.echo('Screenshot for ' + viewport.name + ' (' + viewport.viewport.width + 'x' + viewport.viewport.height + ')', 'info');
-	    this.capture('./public/screenshots/' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height + '.png', {
+	    this.capture('./public/screenshots/' + screenshotDateTime + '/' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height + '.png', {
 	        top: 0,
 	        left: 0,
 	        width: viewport.viewport.width,
 	        height: viewport.viewport.height
 	    });
-	    this.captureSelector('./public/screenshots/' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height + '_full.png', 'body');
+	    this.captureSelector('./public/screenshots/'+ screenshotDateTime + '/' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height + '_full.png', 'body');
   	});
 });
 
 casper.run(function() {
-    this.echo('Finished captures for ' + screenshotUrl).exit();
+    this.echo('Finished captures for ' + screenshotUrl + " " + screenshotDateTime).exit();
 });
--- selenium-ff-and-chrome/routes/postResponsive.js
+++ selenium-ff-and-chrome/routes/postResponsive.js
@@ -5,24 +5,28 @@ var urlencode = require('urlencode');
 var path = require('path');
 var viewports = require('./../const').viewports;
 var imgsList = [];
-for(var i = 0; i < viewports.length; i++){
-	imgsList.push({
-		full:'/screenshots/' + viewports[i].name + '-' + viewports[i].viewport.width + 'x' + viewports[i].viewport.height + '_full.png',
-		screen: '/screenshots/' + viewports[i].name + '-' + viewports[i].viewport.width + 'x' + viewports[i].viewport.height + '.png',
-		sub: viewports[i].name + "-" + viewports[i].viewport.width + 'x' + viewports[i].viewport.height
-	});
-}
 
 var exec = require('child_process').exec;
 router.post('/', function(req, res, next) {
+	var screenshotDateTime = Date.parse(new Date());
+	imgsList.length = 0;
 	let url = req.body.url;
 	url = decodeURIComponent(url);
-	exec('casperjs '+path.join(__dirname, '../casperscreens.js')+' '+ url, function(err,stdout,stderr){
+	exec('casperjs '+path.join(__dirname, '../casperscreens.js') + ' ' + url + ' ' +screenshotDateTime, function(err,stdout,stderr){
 		console.log('stdout: ' + stdout);
+		
 	    if(err) {
 	        console.log('error:'+stderr);
 	    }
 	    else{
+
+	    	for(var i = 0; i < viewports.length; i++){
+				imgsList.push({
+					full:'/screenshots/' + screenshotDateTime + '/' + viewports[i].name + '-' + viewports[i].viewport.width + 'x' + viewports[i].viewport.height + '_full.png',
+					screen: '/screenshots/' + screenshotDateTime + '/' + viewports[i].name + '-' + viewports[i].viewport.width + 'x' + viewports[i].viewport.height + '.png',
+					sub: viewports[i].name + "-" + viewports[i].viewport.width + 'x' + viewports[i].viewport.height
+				});
+			}
 	    	res.send({
     			data:imgsList
     		});

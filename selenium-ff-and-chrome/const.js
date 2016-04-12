fc95b770b157d605d185e6144a2cc743d87d4cd2
cate.zhao <cate.zhao@tpooo.com>
2016-04-12 14:20:29 +0800

    updated types

--- selenium-ff-and-chrome/const.js
+++ selenium-ff-and-chrome/const.js
@@ -4,48 +4,36 @@ const viewports = [
 		'viewport': {width: 240, height: 320}
 	},
 	{
-		'name': 'samsung-galaxy_y-landscape',
-		'viewport': {width: 320, height: 240}
+		'name': 'iphone4-portrait',
+		'viewport': {width: 320, height: 480}
 	},
 	{
 		'name': 'iphone5-portrait',
 		'viewport': {width: 320, height: 568}
 	},
 	{
-		'name': 'iphone5-landscape',
-		'viewport': {width: 568, height: 320}
+		'name': 'iphone6-portrait',
+		'viewport': {width: 375, height: 627}
+	},
+	{
+		'name': 'iphone6s-portrait',
+		'viewport': {width: 414, height: 736}
 	},
 	{
 		'name': 'htc-one-portrait',
 		'viewport': {width: 360, height: 640}
 	},
 	{
-		'name': 'htc-one-landscape',
-		'viewport': {width: 640, height: 360}
-	},
-	{
 		'name': 'nokia-lumia-920-portrait',
 		'viewport': {width: 240, height: 320}
-	},
-	{
-		'name': 'nokia-lumia-920-landscape',
-		'viewport': {width: 320, height: 240}
 	},
 	{
 		'name': 'google-nexus-7-portrait',
 		'viewport': {width: 603, height: 966}
 	},
 	{
-		'name': 'google-nexus-7-landscape',
-		'viewport': {width: 966, height: 603}
-	},
-	{
 		'name': 'ipad-portrait',
 		'viewport': {width: 768, height: 1024}
-	},
-	{
-		'name': 'ipad-landscape',
-		'viewport': {width: 1024, height: 768}
 	},
 	{
 		'name': 'desktop-standard-vga',
@@ -56,20 +44,8 @@ const viewports = [
 		'viewport': {width: 800, height: 600}
 	},
 	{
-		'name': 'desktop-standard-hd',
-		'viewport': {width: 1280, height: 720}
-	},
-	{
 		'name': 'desktop-standard-sxga',
 		'viewport': {width: 1280, height: 1024}
-	},
-	{
-		'name': 'desktop-standard-sxga-plus',
-		'viewport': {width: 1400, height: 1050}
-	},
-	{
-		'name': 'desktop-standard-uxga',
-		'viewport': {width: 1600, height: 1200}
 	},
 	{
 		'name': 'desktop-standard-wuxga',

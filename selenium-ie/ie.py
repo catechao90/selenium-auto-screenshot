0b3249ccb7609c880d1d6d51587c6f01a3da5eb6
cate.zhao <cate.zhao@tpooo.com>
2016-04-12 14:34:17 +0800

    updated code

--- selenium-ie/ie.py
+++ selenium-ie/ie.py
@@ -1,24 +1,10 @@
-
 from selenium import webdriver
-
-from selenium.webdriver.support.ui import WebDriverWait
-
-import time
 import sys 
 
  
 def capture(url, save_fn="./public/IE8.png"):
-
-
-
-    browser = webdriver.Ie() 
-    browser.set_window_size(1920, 900)
+    browser = webdriver.Ie()
     browser.get(url)
- 
-    for i in xrange(10):
-        if "scroll-done" in browser.title:
-            break
-        time.sleep(1)
  
     browser.save_screenshot(save_fn)
     browser.close()
